cd1c8c0a661af17e7a253ec4c34bf2fb9c3f160b
Jenf95 <40010506+Jenf95@users.noreply.github.com>
2019-10-12 12:31:47 -0400

    Update Merge_all_tweets.py

--- Merge_all_tweets.py
+++ Merge_all_tweets.py
@@ -4,11 +4,13 @@ import numpy as np
 import os
 import glob
 import random
+import tweepy
 
 path_merge='C:/Users/Jenny/Desktop/BU COM/Fall 2019/EM 855 Comp Assisted Text Analysis/Research Project/Data/Bernie_Biden_Warren_aug_sep.csv'
 input_path = 'C:/Users/Jenny/Desktop/BU COM/Fall 2019/EM 855 Comp Assisted Text Analysis/Research Project/Data/Raw Data'
 path_sample='C:/Users/Jenny/Desktop/BU COM/Fall 2019/EM 855 Comp Assisted Text Analysis/Research Project/Data/100_sample_Bernie_Biden_Warren_aug_sep.csv'
 path_sample_1000 = 'C:/Users/Jenny/Desktop/BU COM/Fall 2019/EM 855 Comp Assisted Text Analysis/Research Project/Data/1000_sample_Bernie_Biden_Warren_aug_sep.csv'
+path_sample_10000 = 'C:/Users/Jenny/Desktop/BU COM/Fall 2019/EM 855 Comp Assisted Text Analysis/Research Project/Data/10000_sample_Bernie_Biden_Warren_aug_sep.csv'
 
 def concatenate(indir=input_path, outfile=path_merge):
     dfList =[] 
@@ -16,7 +18,7 @@ def concatenate(indir=input_path, outfile=path_merge):
     fileList = glob.glob('*.csv')  
     for filename in fileList:
         print(filename)
-        df=pd.read_csv(filename, skiprows=0)
+        df=pd.read_csv(filename, skiprows=0, encoding=unicode)
         dfList.append(df)
     concateDF = pd.concat(dfList, axis=0, sort=False)
     concateDF.columns = ['id','time','created_at','from_user_name','text','filter_level','possibly_sensitive','withheld_copyright','withheld_scope', \
@@ -24,7 +26,7 @@ def concatenate(indir=input_path, outfile=path_merge):
     	'from_user_id','from_user_realname','from_user_verified','from_user_description','from_user_url','from_user_profile_image_url','from_user_utcoffset',
     	'from_user_timezone','from_user_lang','from_user_tweetcount','from_user_followercount','from_user_friendcount','from_user_favourites_count',
     	'from_user_listed','from_user_withheld_scope','from_user_created_at']
-    concateDF.to_csv(path_merge, mode ='w', sep = ',',index=False)
+    concateDF.to_csv(path_merge, mode ='w', sep = ',',index=False, encoding=unicode)
 
 concatenate()
 
@@ -61,3 +63,10 @@ skip = sorted(random.sample(range(1,n),n-s))
 df = pd.read_csv(path_merge, skiprows = skip)
 
 df.to_csv(path_sample_1000, mode = 'w', index=False)
+
+#select 10000 samples
+n = 18237296
+s = 10000
+skip = sorted(random.sample(range(1,n),n-s))
+df = pd.read_csv(path_merge, skiprows = skip)
+df.to_csv(path_sample_10000, mode = 'w', index = False)
