cb31318d1a7509daf2d8d8f5b91ecefc50abeeba
Jenf95 <40010506+Jenf95@users.noreply.github.com>
2019-11-27 00:04:53 -0500

    Update incivility_tag.R

--- incivility_tag.R
+++ incivility_tag.R
@@ -12,7 +12,6 @@ tbl = fread("C:/Users/Jenny/Desktop/BU COM/Fall 2019/EM 855 Comp Assisted Text A
 tbl = tbl[, c('id', 'time', 'created_at','from_user_name','text','filter_level','lang','to_user_name','in_reply_to_status_id','quoted_status_id','source','location','from_user_id','from_user_realname','from_user_verified','from_user_description','from_user_url','from_user_profile_image_url','from_user_tweetcount','from_user_followercount','from_user_friendcount','from_user_favourites_count','from_user_listed'), with = F]
 
 uncivil_words <- readChar("C:/Users/Jenny/Desktop/BU COM/Fall 2019/EM 855 Comp Assisted Text Analysis/Research Project/Data/cursing_lexicon_in_one_line.txt", nchars = 1e6)
-#words <- 
 tbl$incivility <- ifelse(grepl(uncivil_words,tbl$text, fixed = TRUE),'1','0')
 fwrite(tbl, 'C:/Users/Jenny/Desktop/BU COM/Fall 2019/EM 855 Comp Assisted Text Analysis/Research Project/Data/10000_sample_Bernie_Biden_Warren_aug_sep_tagged.csv')
 summary(tbl)
